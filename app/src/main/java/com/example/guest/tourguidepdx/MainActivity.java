34cf3e656ee6fc796a6f9d7b4e786f3b9b35a7a5
ashleysullins <ashley.sullins@gmail.com>
2015-10-26 10:42:44 -0700

    Add ability to select Let's Go button

--- app/src/main/java/com/example/guest/tourguidepdx/MainActivity.java
+++ app/src/main/java/com/example/guest/tourguidepdx/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.guest.tourguidepdx;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mGoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
 
         // Applying font
         txtExternalfont.setTypeface(tf);
+
+        mGoButton = (Button) findViewById(R.id.goButton);
+
+        mGoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LetsGo.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
